11b0c4ca58a23e82528d9c4286e357404fb44e82
root <root@ip-172-31-44-128.eu-west-3.compute.internal>
2018-11-08 08:17:26 +0000

    added health check on follower init script to exit with right code

--- build-follower-image/init-conjur-follower.sh
+++ build-follower-image/init-conjur-follower.sh
@@ -2,3 +2,5 @@
 
 evoke unpack seed /tmp/seed-follower.tar
 evoke configure follower  
+status_code=$(curl --insecure -I https://localhost/health | head -1 | awk '{print $2}')
+if [ "$status_code" = "200" ]; then exit 0; else exit 1; fi
