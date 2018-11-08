940f650d472c96bdffb11e9070f7ed44afbb9e04
Djamel Bourokba <djamel.bourokba@gmail.com>
2018-11-08 10:50:18 +0000

    added service account definition

--- 2-deploy-follower.sh
+++ 2-deploy-follower.sh
@@ -2,6 +2,8 @@
 conjur_appliance_image=172.30.1.1:5000/conjur-project/conjur-follower:4.9.21
 AUTHENTICATOR_SERVICE_ID=openshift/prod
 
+oc create serviceaccount conjur-cluster -n $CONJUR_NAMESPACE_NAME
+
 sed -e "s#{{ CONJUR_APPLIANCE_IMAGE }}#$conjur_appliance_image#g" ./manifests/conjur-follower.yaml |
   sed -e "s#{{ AUTHENTICATOR_SERVICE_ID }}#$AUTHENTICATOR_SERVICE_ID#g" |
   oc create -f -
