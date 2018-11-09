f564ef53f82a3db1d26987fdaad794803a6b53c9
Cloud User <centos@ip-172-31-43-17.eu-west-3.compute.internal>
2018-11-09 12:52:36 +0000

    Fixes

--- 06-build-and-push-follower-image.sh
+++ 06-build-and-push-follower-image.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
-path_to_tarball="./conjur-follower-image/conjur-follower-4.9.21.0.tar"
+. vars.sh
+
+path_to_tarball="./build-follower-image/conjur-appliance-4.9.21.0.tar.xz"
 docker load -i $path_to_tarball
-docker tag conjur-follower:4.9.21 172.30.1.1:5000/conjur-project/conjur-follower:4.9.21
+pushd build-follower-image
+  ./build.sh
+popd
+docker tag conjur-follower:4.9.21 172.30.1.1:5000/$CONJUR_NAMESPACE_NAME/conjur-follower:4.9.21
 docker login -u developer -p $(oc whoami -t) 172.30.1.1:5000
-docker push 172.30.1.1:5000/conjur-project/conjur-follower
+docker push 172.30.1.1:5000/$CONJUR_NAMESPACE_NAME/conjur-follower:4.9.21
 
--- 1-create-master-service.sh
+++ 1-create-master-service.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 . vars.sh
-oc create -f ./manifests/conjur-master-service.yaml 
-oc create -f ./manifests/conjur-master-endpoint.yaml
+sed -e "s#{{ CONJUR_NAMESPACE_NAME }}#$CONJUR_NAMESPACE_NAME#g" ./manifests/conjur-master-service.yaml | oc create -f -
 sed -e "s#{{ CONJUR_MASTER_IP }}#$conjur_master_ip#g" ./manifests/conjur-master-endpoint.yaml | oc create -f -
+
 
