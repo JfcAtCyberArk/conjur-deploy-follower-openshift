8f3885859765c53be1263886efd632e6f0d5810f
JfcAtCyberArk <41334195+JfcAtCyberArk@users.noreply.github.com>
2018-11-09 11:31:37 +0100

    Use variable in oc project creation

--- 05-create-oc-project.sh
+++ 05-create-oc-project.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-oc new-project conjur-project
+
+. vars.sh
+
+oc new-project $CONJUR_NAMESPACE_NAME
 oc login -u system:admin
 oc adm policy add-scc-to-group anyuid system:authenticated
 oc login -u developer -p password
