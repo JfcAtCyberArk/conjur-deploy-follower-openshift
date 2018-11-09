a9b21c5ee03f445b813e69039afc478b70df5f84
JfcAtCyberArk <41334195+JfcAtCyberArk@users.noreply.github.com>
2018-11-09 16:22:18 +0100

    Update 05-create-oc-project.sh
    Load conjur-authenticator-role

--- 05-create-oc-project.sh
+++ 05-create-oc-project.sh
@@ -5,5 +5,6 @@
 oc new-project $CONJUR_NAMESPACE_NAME
 oc login -u system:admin
 oc adm policy add-scc-to-group anyuid system:authenticated
+oc create -f ./manifests/conjur-auhenticator-role.yaml
 oc login -u developer -p password
 
